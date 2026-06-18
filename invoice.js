8e295a80f29408bc2f61b6eb7f56d0474bbade13
Rahul Kumar 🚀 <56966500+rahulkumarpahwa@users.noreply.github.com>
2026-06-18 06:02:58 +0000

    feat: created the db statement to get the required data.

--- invoice.js
+++ invoice.js
@@ -2,14 +2,37 @@ import sqlite3 from "sqlite3";
 
 // connect to the database here
 // data.db is the name of the database file
+const db = new sqlite3.Database("./data.db")
 
 export default function registerInvoice(fastify, opts, done) {
   fastify.all("/", (request, reply) => {
     const id = request.query.id;
+    console.log(id)
 
     // code goes here
-
-    reply.send({ invoice: {}, lines: [] });
+    db.get(
+      "SELECT * FROM invoice WHERE InvoiceId = ?",
+      [id],
+      (err, invoice) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(invoice)
+        db.all(
+          "SELECT * FROM invoiceline WHERE InvoiceId = ?",
+          [id],
+          (err, lines) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(lines);
+            reply.send({ invoice, lines });
+          }
+        );
+      }
+    );
   });
 
   done();
@@ -23,8 +46,8 @@ this is how the response should look (to work in the UI)
       "id":10,
       "date":"2021-02-03 00:00:00",
       "address":"3 Chatham Street",
-      "city":"Dublin","state":
-      "Dublin",
+      "city":"Dublin",
+      "state":On which port does the local development server run?"Dublin",
       "country":"Ireland",
       "postalCode":null,
       "total":5.94
